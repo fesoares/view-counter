f6c1adc3b51c0f988fcdaab130916f71686b3c72
Felipe Soares <felipe4dev@gmail.com>
2021-08-21 01:08:26 -0300

    update requests

--- components/pdf-viewer.js
+++ components/pdf-viewer.js
@@ -3,14 +3,43 @@ import React, { useState, useEffect } from "react";
 export default function Viewer() {
 
   useEffect(() => {
-    console.log('1')
+    fetch('https://api-view-counter.herokuapp.com/counter/create', { 
+      method: 'POST', 
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ type: 'view', created_at: new Date() }) 
+    }).then(response => {
+      console.log(response);
+    }).catch(error => {
+      console.log(error);
+    })
   }, []);
+
+  const openDocument = async () => {
+    fetch('https://api-view-counter.herokuapp.com/counter/create', { 
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ type: 'click', created_at: new Date() }) 
+    }).then(response => {
+      console.log(response);
+    }).catch(error => {
+      console.log(error);
+    })
+  }
 
   return (
     <div>
-      <a href={"https://drive.google.com/file/d/1dGtop1Z20EyRtBihFbasW6T5DmLn07q2/view?usp=sharing"} target='_blank' rel="noreferrer">
-        <p>Click to open PDF file in a new tab</p>
-      </a>
+      <button onClick={() => openDocument()}>
+        <p>Clique aqui para acessar o PDF!</p>
+      </button>
+      {/* <a href={"https://drive.google.com/file/d/1dGtop1Z20EyRtBihFbasW6T5DmLn07q2/view?usp=sharing"} target='_blank' rel="noreferrer">
+        
+      </a> */}
     </div>
   );
 }

5e31b22afccdfc87d2bcf4c7c4de8a0abce83f39
Felipe Soares <felipe4dev@gmail.com>
2021-11-29 21:42:28 -0300

    update pdf

--- components/pdf-viewer.js
+++ components/pdf-viewer.js
@@ -38,6 +38,6 @@ export default function Viewer() {
 //       <iframe src="https://drive.google.com/file/d/1fDgHcvKCG3BzeCPkLnbg35emri7jsoAm/preview" width="640" height="480" allow="autoplay"></iframe>
 //       <a ref={linkRef} href="https://drive.google.com/file/d/1fDgHcvKCG3BzeCPkLnbg35emri7jsoAm/view?usp=sharing" target="_blank" onClick={() => openDocument()} rel="noreferrer">Clique aqui para acessar o PDF!</a>
 //     </div>
-    <iframe src="https://drive.google.com/file/d/1fDgHcvKCG3BzeCPkLnbg35emri7jsoAm/preview" width="100%" height="100%" allow="autoplay"></iframe>
+    <iframe src="https://drive.google.com/file/d/17bXWj20lw-h21VkDl_X5HpUcc6pPlVWX/preview" width="100%" height="100%" allow="autoplay"></iframe>
   );
 }
